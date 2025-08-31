d3810888f25819b627fdc8943423cc3687678835
Exepa <exepa@hotmail.com>
2025-08-31 10:34:09 -0300

    Add: Cmake buil

--- main.cpp
+++ main.cpp
@@ -1,49 +1,47 @@
-#include <iostream>
+#include <cstdlib>
 #include <fstream>
-
+#include <iostream>
+#include <stdint.h>
 
 struct BMPHeader {
-	char signature[2];
-	uint32_t filesize;
-	uint32_t reserved;
-	uint32_t dataoffset;
+  char signature[2];
+  uint32_t filesize;
+  uint32_t reserved;
+  uint32_t dataoffset;
 };
 
-int main(int argc, char* argv[])
-{
-	std::cout << argv[1] << std::endl;
-	std::ifstream file(argv[1], std::ios::binary);
-	if(!file){
-		std::cout << "FileOpenError" << std::endl;
-		return 1;
-	}
-	
-	file.seekg(0, std::ios::beg);
-	BMPHeader bmp;
-	char bufferSing[2] = {};
-	uint32_t bufferSize;
-	uint32_t bufferReser;
-	uint32_t bufferOffset;
-	file.read(bufferSing,2);
-	bmp.signature[0] = bufferSing[0];
-	bmp.signature[1] = bufferSing[1];
-	file.read((char*)&bufferSize,4);
-	bmp.filesize = bufferSize;
-	file.read((char*)&bufferReser,4);
-	bmp.reserved = bufferReser;
-	file.read((char*)&bufferOffset,4);
-	bmp.dataoffset = bufferOffset;
-	std::cout << "Signature: " << bmp.signature;
-	std::cout << std::endl;
-	std::cout << "FileSize: " << bmp.filesize;
-	std::cout << std::endl;
-	std::cout << "Reserved: " << bmp.reserved;
-	std::cout << std::endl;
-	std::cout << "DataOffset: " << bmp.dataoffset;
-	std::cout << std::endl;
+int main(int argc, char *argv[]) {
+  std::cout << argv[1] << std::endl;
+  std::ifstream file(argv[1], std::ios::binary);
+  if (!file) {
+    std::cout << "FileOpenError" << std::endl;
+    return 1;
+  }
 
-	
+  file.seekg(0, std::ios::beg);
+  BMPHeader bmp;
+  char bufferSing[2] = {};
+  uint32_t bufferSize;
+  uint32_t bufferReser;
+  uint32_t bufferOffset;
+  file.read(bufferSing, 2);
+  bmp.signature[0] = bufferSing[0];
+  bmp.signature[1] = bufferSing[1];
+  file.read((char *)&bufferSize, 4);
+  bmp.filesize = bufferSize;
+  file.read((char *)&bufferReser, 4);
+  bmp.reserved = bufferReser;
+  file.read((char *)&bufferOffset, 4);
+  bmp.dataoffset = bufferOffset;
+  std::cout << "Signature: " << bmp.signature;
+  std::cout << std::endl;
+  std::cout << "FileSize: " << bmp.filesize;
+  std::cout << std::endl;
+  std::cout << "Reserved: " << bmp.reserved;
+  std::cout << std::endl;
+  std::cout << "DataOffset: " << bmp.dataoffset;
+  std::cout << std::endl;
 
-	file.close();
-	return 0;
-} 
+  file.close();
+  return 0;
+}
